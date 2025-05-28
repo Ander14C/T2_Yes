def114ed771f0b0b21e9bee652bbaaf42c007c8b
Nygs <nygs@upn.pe>
2025-05-28 17:00:09 -0500

    Agregar archivos de proyecto.

---
+++ Biblio/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio
+{
+    public class Class1
+    {
+        int r, c, d, a;
+        int monto = 1000;
+
+        public void retiro()
+        {
+            Console.WriteLine("Ingrese el monto a retirar: ");
+            int r = int.Parse(Console.ReadLine());
+            if (r <= 500)
+            {
+                if (c >= 0)
+                {
+                    Console.WriteLine("Realizando operación. Espere un momento...");
+                    Console.WriteLine("Su retiro ha sido realizado con éxito");
+                    Console.WriteLine("Retiro con comisión de S/ 8.00: " + c);
+                }
+                else
+                {
+                    Console.WriteLine("Monto insuficiente");
+                }
+                monto = c;
+            }
+            else
+            {
+                Console.WriteLine("La operación no puede ser mayor a S/500");
+            }
+            if (r < 0)
+            {
+                Console.WriteLine("El monto ingresado no es valido");
+            }
+            else
+            {
+                c = monto - (r + 8);
+                Console.WriteLine("Realizando operación. Espere un momento...");
+                Console.WriteLine("Su retiro ha sido realizado con éxito");
+                Console.WriteLine("Retiro con comisión de S/ 8.00: " + c);
+            }
+        }
+        public void saldo()
+        {
+            Console.WriteLine("El monto actual es:" + monto);
+        }
+        public void deposito()
+        {
+            Console.WriteLine("Ingrese el monto a depositar (Costo de la comision S/.2)");
+            d = int.Parse(Console.ReadLine());
+
+            if (d <= 1000)
+            {
+                a = monto + (d - 2);
+                Console.WriteLine("Se ha depositado: " + d + " menos 2 soles de comisión");
+                Console.WriteLine("El monto actual es: " + a);
+            }
+            else
+            {
+                Console.WriteLine("La operación no puede ser mayor a S/1000");
+            }
+        }
+    }
+}
---
+++ T2_Yes/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Biblio;
+
+namespace T2_Yes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 Cajero = new Class1();
+            int opcion;
+            do
+            {
+                Console.Clear(); //Limpia los valores de la consola
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("--- MENU CAJERO AUTOMÁTICO ---");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("1. Retirar");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("2. Depositar");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("3. Consultar Saldo");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("4. Salir");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Seleccione una opción: ");
+                opcion = int.Parse(Console.ReadLine());
+                opcion = int.Parse(Console.ReadLine());
+
+
+
+                switch (opcion)
+
+
+
+                {
+
+
+
+                    case 0:
+
+
+
+                        Console.WriteLine("saliendo....");
+
+
+
+                        break;
+
+
+
+                    case 1:
+
+
+
+                        Cajero.retiro(); ;
+
+
+
+                        break;
+
+
+
+                    case 2:
+
+                        Cajero.saldo();
+                        break;
+                    case 3:
+                        Cajero.deposito();
+                        break;
+                    default:
+                        Console.WriteLine("opcion no valida");
+                        break;
+                }        
+            }
+            while (opcion !=0);
+        }
+    }
+    
+}
